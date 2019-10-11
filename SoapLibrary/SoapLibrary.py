3780e27eda3490c8820b9dca66996d6667c3bad4
jdagomes <joaodaniel.gomes@altran.com>
2019-10-11 09:07:40 +0100

    keyword fixes

--- SoapLibrary/SoapLibrary.py
+++ SoapLibrary/SoapLibrary.py
@@ -1,5 +1,6 @@
 import os
 import logging.config
+from config import DICT_CONFIG
 from requests import Session
 from zeep import Client
 from zeep.transports import Transport
@@ -7,28 +8,11 @@ from zeep.wsdl.utils import etree
 from robot.api import logger
 from robot.api.deco import keyword
 
-logging.config.dictConfig({
-    'version': 1,
-    'formatters': {
-        'verbose': {
-            'format': '%(name)s: %(message)s'
-        }
-    },
-    'handlers': {
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-            'formatter': 'verbose',
-        },
-    },
-    'loggers': {
-        'zeep.transports': {
-            'level': 'DEBUG',
-            'propagate': True,
-            'handlers': ['console'],
-        },
-    }
-})
+logging.config.dictConfig(DICT_CONFIG)
+
+DEFAULT_HEADERS = {'Content-Type': 'text/xml; charset=utf-8'}
+LOG_HEADER_RESPONSE_FROM_WS = '<font size="3"><b>Response from webservice:</b></font> '
+LOCAL_NAME_XPATH = "//*[name()='%s']"
 
 
 class SoapLibrary:
@@ -47,31 +31,30 @@ class SoapLibrary:
         self.url = url
         session = Session()
         self.client = Client(self.url, transport=Transport(session=session))
-        logger.info('Connected to: ' + self.client.wsdl.location)
+        logger.info('Connected to: %s' % self.client.wsdl.location)
         info = self.client.service.__dict__
         operations = info["_operations"]
-        logger.info('Available operations: ' + str(operations.keys()))
+        logger.info('Available operations: %s' % operations.keys())
 
     @keyword("Call SOAP Method With XML")
-    def call_soap_method_xml(self, xml):
+    def call_soap_method_xml(self, xml, headers=DEFAULT_HEADERS):
         """ Send the informed XML to the SOAP client. You only need to inform the path to the Request XML, the SOAP
             method are inside the XML file.
 
             Example:
                 | ${response} | Call SOAP Method With XML |  C:\\Request.xml |
         """
-        headers = {'Content-Type': 'text/xml; charset=utf-8'}
-        #            'SOAPAction': self.url + '/%s' % method}
+        # TODO check with different headers: 'SOAPAction': self.url + '/%s' % method}
         raw_text_xml = self._convert_xml_to_raw_text(xml)
         xml_obj = etree.fromstring(raw_text_xml)
         response = self.client.transport.post_xml(address=self.url, envelope=xml_obj, headers=headers)
         etree_response = self._parse_from_unicode(response.text)
         status_code = response.status_code
         if status_code != 200:
-            logger.debug('URL: ' + str(response.url))
+            logger.debug('URL: %s' % response.url)
             logger.debug(etree.tostring(etree_response, pretty_print=True, encoding='unicode'))
-            raise AssertionError('Request Error! Status Code: ' + str(status_code) + '! Reason: ' + str(response.reason))
-        logger.info('<font size="3"><b>Response from webservice:</b></font> ', html=True)
+            raise AssertionError('Request Error! Status Code: %s! Reason: %s' % (status_code, response.reason))
+        logger.info(LOG_HEADER_RESPONSE_FROM_WS, html=True)
         logger.info(etree.tostring(etree_response, pretty_print=True, encoding='unicode'))
         return etree_response
 
@@ -93,14 +76,14 @@ class SoapLibrary:
         if isinstance(data_list, (float, int)):
             return int(data_list)
         if len(data_list) == 0:
-            logger.warn('The search "' + xpath + '" did not return any result! Please confirm the tag!')
+            logger.warn('The search "%s" did not return any result! Please confirm the tag!' % xpath)
         if len(data_list) > 1:
-            logger.warn('The tag you entered found ' + str(len(data_list)) + ' items, returning the text in the index '
-                        'number ' + str(new_index) + ', if you want a different index inform the argument index=N')
+            logger.warn('The tag you entered found %s items, returning the text in the index '
+                        'number %s, if you want a different index inform the argument index=N' % (len(data_list), new_index))
         return data_list[new_index].text
 
     @keyword("Edit XML Request")
-    def edit_xml(self, xml_filepath, new_values_dict, request_name):
+    def edit_xml(self, xml_file_path, new_values_dict, request_name):
         """ Changes a field on the given XML to a new given value, the values must be in a dictionary.
             xml_filepath must be a "template" of the request to the webservice.
             new_values_dict must be a dictionary with the keys and values to change.
@@ -113,25 +96,25 @@ class SoapLibrary:
                 | ${xml_edited} | Edit XML Request | request_filepath | ${dict} | New_Request |
                 | ${response} | Call SOAP Method With XML | ${xml_edited} |
         """
-        string_xml = self._convert_xml_to_raw_text(xml_filepath)
+        string_xml = self._convert_xml_to_raw_text(xml_file_path)
         xml = self._convert_string_to_xml(string_xml)
         if not isinstance(new_values_dict, dict):
-            raise Exception("new_values_dict must be a dictionary")
+            raise Exception("new_values_dict argument must be a dictionary")
         for key, value in new_values_dict.iteritems():
-            if len(xml.xpath("//*[name()='%s']" % key)) == 0:
-                logger.warn('Tag ' + key + ' not found')
+            if len(xml.xpath(LOCAL_NAME_XPATH % key)) == 0:
+                logger.warn('Tag "%s" not found' % key)
                 continue
-            xml.xpath("//*[name()='%s']" % key)[0].text = value
+            xml.xpath(LOCAL_NAME_XPATH % key)[0].text = value
         # Create new file with replaced request
         new_file_name = str(request_name) + ".xml"
-        new_file_path = os.path.join(os.path.dirname(xml_filepath), new_file_name)
+        new_file_path = os.path.join(os.path.dirname(xml_file_path), new_file_name)
         request_file = open(new_file_path, 'wb')
         request_file.write(etree.tostring(xml))
         request_file.close()
         return new_file_path
 
     @keyword("Save XML Response File")
-    def save_xml_response_file(self, response, response_name):
+    def save_xml_response_file(self, response, save_folder, response_name):
         """ Save the webservice response as a XML file.
 
             Returns the file path of the new Request file.
@@ -140,7 +123,7 @@ class SoapLibrary:
                 | ${response_file} | Save XML Response File |  ${response} | Response_file |
         """
         new_file_name = str(response_name) + ".xml"
-        new_file_path = os.path.join(os.getcwd(), new_file_name)
+        new_file_path = os.path.join(save_folder, new_file_name)
         request_file = open(new_file_path, 'wb')
         # TODO Fix the encoding in the generated file
         request_file.write(etree.tostring(response, pretty_print=True))
@@ -204,9 +187,9 @@ class SoapLibrary:
         xpath = ''
         if isinstance(xpath_list, list):
             for el in xpath_list:
-                xpath += "//*[name()='%s']" % el
+                xpath += LOCAL_NAME_XPATH % el
         else:
-            xpath += "//*[name()='%s']" % xpath_list
+            xpath += LOCAL_NAME_XPATH % xpath_list
         return xpath
 
     @staticmethod
