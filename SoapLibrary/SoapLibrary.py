8f426e747c80613957df78804cc517f950a9d500
Robin Matz <robin.s.matz@gmail.com>
2021-12-27 07:00:00 +0100

    enable HTTP Basic Authentication

--- SoapLibrary/SoapLibrary.py
+++ SoapLibrary/SoapLibrary.py
@@ -4,6 +4,7 @@ import warnings
 import base64
 from .config import DICT_CONFIG
 from requests import Session
+from requests.auth import HTTPBasicAuth
 from zeep import Client
 from zeep.transports import Transport
 from zeep.wsdl.utils import etree
@@ -31,7 +32,7 @@ class SoapLibrary:
         self.url = None
 
     @keyword("Create SOAP Client")
-    def create_soap_client(self, url, ssl_verify=True, client_cert_location=None):
+    def create_soap_client(self, url, ssl_verify=True, client_cert_location=None, auth=None):
         """
         Loads a WSDL from the given ``url`` and creates a Zeep client.
         List all Available operations/methods with INFO log level.
@@ -46,18 +47,21 @@ class SoapLibrary:
         If your host requires client certificate based authentication, you can pass the
         path to your client certificate to the ``client_cert_path`` argument.
 
+        For HTTP Basic Authentication, you can pass the list with username and password
+        to the ``auth`` parameter.
+
         *Example:*
-        | Create SOAP Client | http://endpoint.com?wsdl  |
-        | Create SOAP Client | https://endpoint.com?wsdl | ssl_verify=True |
-        | Create SOAP Client | https://endpoint.com?wsdl | client_cert_location=${CURDIR}${/}mycert.pem |
+        | Create SOAP Client    | http://endpoint.com?wsdl  |
+        | Create SOAP Client    | https://endpoint.com?wsdl | ssl_verify=True                               |
+        | Create SOAP Client    | https://endpoint.com?wsdl | client_cert_location=${CURDIR}${/}mycert.pem  |
+        | ${auth}               | Create List               | username | password                           |
+        | Create SOAP Client    | https://endpoint.com?wsdl | auth=${auth}                                  |
         """
         self.url = url
         session = Session()
-        if is_falsy(ssl_verify):
-            session.verify = False
-        elif is_string(ssl_verify):
-            session.verify = ssl_verify
+        session.verify = ssl_verify
         session.cert = client_cert_location
+        session.auth = HTTPBasicAuth(*auth) if auth else None
         self.client = Client(self.url, transport=Transport(session=session))
         logger.info('Connected to: %s' % self.client.wsdl.location)
         info = self.client.service.__dict__
